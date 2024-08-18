917007ad0ecf2ed3aeedb273268f4e209d4c2314
Aman kumar <amank0259@gmail.com>
2024-08-19 00:27:33 +0530

    add preloader

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ThemeContextProvider from './contexts/ThemeContext';
 import { Route, Routes } from 'react-router-dom';
 import Home from './components/Home'
 import ActiveSectionContextProvider from './contexts/activeSessionContext';
+import Loader from './ui/Loader';
 
 const App = () => {
-  return (
-    <ThemeContextProvider>
-      <ActiveSectionContextProvider>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </ActiveSectionContextProvider>
-    </ThemeContextProvider>
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2600);
+    return () => clearTimeout(timer);
+  }, []);
+  return (<>
+    {loading ? <Loader /> :
+      <>
+        <ThemeContextProvider>
+          <ActiveSectionContextProvider>
+            <Routes>
+              <Route path="/" element={<Home />} />
+            </Routes>
+          </ActiveSectionContextProvider>
+        </ThemeContextProvider>
+      </>}
+  </>
+
   )
 }
 
